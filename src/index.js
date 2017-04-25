68d90731f437c5d0ffef26f97586fdaaa9ad5c92
Christian Grøngaard <christianhg@gmail.com>
2017-04-25 19:39:52 +0200

    🦄

---
+++ src/index.js
@@ -0,0 +1,80 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import axios from 'axios'
+import { compose, flatten, map, prop, reverse, sortBy, take } from 'bukk'
+const Task = require('data.task')
+
+const httpGet = url =>
+  new Task((rej, res) => axios.get(url).then(res).catch(rej))
+const getJson = url => httpGet(url).map(res => res.data)
+const getSets = () => getJson('http://mtgjson.com/json/AllSetsArray.json')
+const getCards = () => getSets().map(map(prop('cards'))).map(flatten)
+
+const getLength = s => s.length
+const sortByNameLength = sortBy(compose(getLength, prop('name')))
+const takeTen = take(10)
+const cardsWithLongestNames = compose(takeTen, reverse, sortByNameLength)
+const cardsWithShortestNames = compose(takeTen, sortByNameLength)
+const cardListView = card => <li key={card.id}>{card.name}</li>
+const calculations = [
+  {
+    id: 0,
+    title: 'Cards with longest names',
+    cards: []
+  },
+  {
+    id: 1,
+    title: 'Cards with shortest names',
+    cards: []
+  }
+]
+
+function Calculation(props) {
+  return (
+    <div key={props.id}>
+      <h2>{props.title}:</h2>
+      <ol>
+        {props.cards.map(cardListView)}
+      </ol>
+    </div>
+  )
+}
+
+class Magicalc extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      calculations
+    }
+  }
+
+  componentDidMount() {
+    getCards().fork(console.error, cards =>
+      this.setState(prevState => ({
+        calculations: [
+          {
+            ...prevState.calculations[0],
+            cards: cardsWithLongestNames(cards)
+          },
+          {
+            ...prevState.calculations[1],
+            cards: cardsWithShortestNames(cards)
+          }
+        ]
+      }))
+    )
+  }
+
+  render() {
+    const calculations = this.state.calculations.map(Calculation)
+
+    return (
+      <div>
+        <h1>Magicalc</h1>
+        {calculations}
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<Magicalc />, document.getElementById('root'))
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+
+module.exports = {
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    historyApiFallback: false,
+    port: 9000
+  },
+  entry: {
+    index: './src/index.js'
+  },
+  output: {
+    filename: '[name].[chunkhash].js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        exclude: /node_modules/,
+        test: /\.js$/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ]
+}
